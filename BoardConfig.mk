5285dc8bf6c89cd2a8921233ab90422bcd3bf120
赵元望 <34432603+zhaoyuanwang123@users.noreply.github.com>
2026-09-06 04:05:10 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,8 +21,8 @@ BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 17179869184
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm system_dlkm odm cust preas preavs version preload
 
 # 预编译内核与 DTB
-TARGET_PREBUILT_KERNEL := device/honor/rod2/prebuilt/kernel
-TARGET_PREBUILT_DTB := device/honor/rod2/prebuilt/dtb.img
+TARGET_PREBUILT_KERNEL := kernel
+TARGET_PREBUILT_DTB := dtb.img
 
 # 内核 cmdline（从 /proc/cmdline 提取，可稍作精简但保留关键项）
 BOARD_KERNEL_CMDLINE := console=ttynull stack_depot_disable=on cgroup_disable=pressure kasan.page_alloc.sample=10 kasan.stacktrace=off kvm-arm.mode=protected bootconfig ioremap_guard loglevel=6 kpti=0 log_buf_len=256K swiotlb=noforce loop.max_part=7 pcie_ports=compat irqaffinity=0-2 cpufreq.default_governor=performance sysctl.kernel.sched_pelt_multiplier=4 kasan=off cgroup.memory=nokmem,nosocket disable_dma32=on kernel.panic_on_rcu_stall=1 fw_devlink.strict=1 service_locator.enable=1 rcupdate.rcu_expedited=1 rcu_nocbs=0-7 no-steal-acc printk.console_no_auto_verbose=1 can.stats_timer=0 ftrace_dump_on_oops firmware_class.path=/vendor/firmware_mnt/image,/vendor/firmware_mnt/image/kiwi,/vendor/vm-system/oemvm/boot,/vendor/vm-system/trustedvm/boot,/vendor/firmware,/vendor/firmware/wlan/qca_cld/kiwi_v2 pci-msm-drv.pcie_sm_regs=0x1D07000,0x1040,0x1048,0x3000,0x1 video=vfb:640x400,bpp=32,memsize=3072000 cpufreq.default_governor=performance bootconfig buildvariant=user
