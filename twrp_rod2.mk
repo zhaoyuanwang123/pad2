d3b04a4b279061917b17145e30be7f0ce368a72e
赵元望 <34432603+zhaoyuanwang123@users.noreply.github.com>
2026-09-06 04:13:09 +0800

    Create twrp_rod2.mk

---
+++ twrp_rod2.mk
@@ -0,0 +1,8 @@
+# 继承设备基本配置
+$(call inherit-product, device/honor/rod2/device.mk)
+
+PRODUCT_NAME := twrp_rod2
+PRODUCT_DEVICE := rod2
+PRODUCT_BRAND := Honor
+PRODUCT_MODEL := MagicPad 2
+PRODUCT_MANUFACTURER := Honor
